1057b474a6fd87937c27662c8939d2b417294c6c
ShkiTTer <shkipper65@gmail.com>
2019-10-10 12:34:23 +0700

    Add splash screen

--- app/src/main/java/com/example/todo/LoginActivity.kt
+++ app/src/main/java/com/example/todo/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.todo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setTheme(R.style.AppTheme)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.apply {
+            lifecycleOwner = this@LoginActivity
+        }
     }
 }
