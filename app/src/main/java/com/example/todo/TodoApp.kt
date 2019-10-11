1131a047de01ab6f74447d7d7b09d880b5fc0507
Maksim Sachuk <shkipper65@gmail.com>
2019-10-11 21:10:47 +0700

    Start koin module

--- app/src/main/java/com/example/todo/TodoApp.kt
+++ app/src/main/java/com/example/todo/TodoApp.kt
@@ -4,9 +4,11 @@ import android.app.Application
 import com.example.todo.data.network.repository.NetworkRepository
 import com.example.todo.data.network.utils.TaskApiProvider
 import com.example.todo.data.repository.TaskRepository
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
-class TodoApp: Application() {
+class TodoApp : Application() {
     private val koinModule = module {
         single { TaskApiProvider.create() }
         single { NetworkRepository(get()) }
@@ -16,5 +18,11 @@ class TodoApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidContext(applicationContext)
+            modules(koinModule)
+            printLogger()
+        }
     }
 }
