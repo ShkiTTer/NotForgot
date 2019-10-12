6d2051614cf39806d3f661d4c72acbdd8fd66fc9
Maksim Sachuk <shkipper65@gmail.com>
2019-10-12 12:09:33 +0700

    Use mapper

--- app/src/main/java/com/example/todo/data/network/repository/NetworkRepository.kt
+++ app/src/main/java/com/example/todo/data/network/repository/NetworkRepository.kt
@@ -5,7 +5,6 @@ import com.example.todo.data.network.entity.User
 import com.example.todo.data.repository.INetworkRepository
 
 class NetworkRepository(private val taskApiService: TaskApiService) : INetworkRepository {
-    override fun registerUser(registerUser: RegisterUser): User {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun registerUser(registerUser: RegisterUser): User =
+        taskApiService.registerUser(registerUser)
 }
--- app/src/main/java/com/example/todo/data/repository/TaskRepository.kt
+++ app/src/main/java/com/example/todo/data/repository/TaskRepository.kt
@@ -1,11 +1,18 @@
 package com.example.todo.data.repository
 
+import com.example.todo.data.mapper.NetworkMapper
 import com.example.todo.domain.entity.NewUser
 import com.example.todo.domain.entity.User
 import com.example.todo.domain.repository.ITaskRepository
 
-class TaskRepository(private val networkRepository: INetworkRepository): ITaskRepository {
+class TaskRepository(private val networkRepository: INetworkRepository) : ITaskRepository {
     override fun registerUser(newUser: NewUser): User {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return NetworkMapper.userFromNetwork(
+            networkRepository.registerUser(
+                NetworkMapper.newUserToNetwork(
+                    newUser
+                )
+            )
+        )
     }
 }
