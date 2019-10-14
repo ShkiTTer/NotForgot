16fd2c72e5c2aacdddcfa192927cd9d8cb080d1b
Maksim Sachuk <shkipper65@gmail.com>
2019-10-14 21:41:10 +0700

    Enable data binding in MainActivity

--- app/src/main/java/com/example/todo/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/todo/presentation/ui/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.todo.presentation.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.todo.R
+import com.example.todo.databinding.ActivityMainBinding
+import com.example.todo.presentation.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            tasks = mainViewModel.tasks
+        }
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.fabAdd.setOnClickListener {
+            TODO("Add activity for add or edit task")
+        }
     }
 }
