33ecba43b4f42724bae14b2d57def58f360108c7
Maksim Sachuk <shkipper65@gmail.com>
2019-10-21 14:01:11 +0700

    Add date fields

--- app/src/main/java/com/example/todo/data/db/entity/Task.kt
+++ app/src/main/java/com/example/todo/data/db/entity/Task.kt
@@ -1,15 +1,18 @@
 package com.example.todo.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity
 data class Task(
     val title: String,
     val description: String,
-    val created: Int,
-    val deadline: Int?,
+    val created: Date,
+    val deadline: Date?,
     val category: Category,
     val priority: Priority,
     val done: Int,
-    val synchronized: Boolean
+    val synchronized: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
 )
